87630741fd54f621e6c6633cde6f3fc520292e9c
Wadih Ojeil <wadih.ojeil@gmail.com>
2021-10-27 19:39:15 -0400

    updated web3 to work whether a browser contains meta mask or not

--- ethereum/web3.js
+++ ethereum/web3.js
@@ -1,7 +1,17 @@
 import Web3 from "web3";
  
-window.ethereum.request({ method: "eth_requestAccounts" });
+let web3;
  
-const web3 = new Web3(window.ethereum);
+if (typeof window !== "undefined" && typeof window.ethereum !== "undefined") {
+  // We are in the browser and metamask is running.
+  window.ethereum.request({ method: "eth_requestAccounts" });
+  web3 = new Web3(window.ethereum);
+} else {
+  // We are on the server *OR* the user is not running metamask
+  const provider = new Web3.providers.HttpProvider(
+    "https://rinkeby.infura.io/v3/c3233d34ba6446749916d2373f37a87e"
+  );
+  web3 = new Web3(provider);
+}
  
 export default web3;
