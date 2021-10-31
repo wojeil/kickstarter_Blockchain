caa423de5d20454daa6197b60a53c1f8e77dc25a
Wadih Ojeil <wadih.ojeil@gmail.com>
2021-10-31 13:05:46 -0400

    added layouts and integrating components to pages

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import { Card, Button } from 'semantic-ui-react';
 import factory from '../ethereum/factory';
+import Layout from '../components/Layout';
 
 
 
