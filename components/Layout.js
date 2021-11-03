d14575a15c9b16783da8c9847e020990da1c32e9
Wadih Ojeil <wadih.ojeil@gmail.com>
2021-11-02 20:12:02 -0400

    imported the container to fix UI

--- components/Layout.js
+++ components/Layout.js
@@ -1,13 +1,14 @@
 import React from "react";
 import Header from "./Header";
+import {Container} from "semantic-ui-react";
  
 const Layout = (props) => {
     return (
-        <div>
+        <Container>
             <Header/>
             {props.children}
             
-        </div>
+        </Container>
     )
 };
 export default Layout;
