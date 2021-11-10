de0154f93b700263b6b3b2290ddc73953bd64dba
Wadih Ojeil <wadih.ojeil@gmail.com>
2021-11-09 20:54:07 -0500

    created a form for the Create campaign page and lleveraging semantic ui react

--- pages/campaigns/new.js
+++ pages/campaigns/new.js
@@ -1,4 +1,5 @@
 import React, {Component} from "react";
+import {Form, Button } from 'semantic-ui-react';
 import Layout from "../../components/Layout";
 
 
@@ -8,7 +9,17 @@ class CampaignNew extends  Component {
         return(
 
             <Layout>
-                <h1>New Campain</h1>
+               <h3> Create a campaign </h3>
+
+                <Form>
+                    <Form.Field>
+                        <label> Minimum Contribution</label>
+                        <input />
+                    </Form.Field>
+
+                    <Button primary> Create </Button>
+
+                </Form>
             </Layout>
          
 
