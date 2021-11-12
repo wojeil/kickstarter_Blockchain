0aa581e3170f5a71b0de6bdcd915e50493c3973c
Wadih Ojeil <wadih.ojeil@gmail.com>
2021-11-11 19:16:26 -0500

    installed and setting up rout (helper)

---
+++ routes.js
@@ -0,0 +1,7 @@
+
+//require returns a function and the set of paranthesis after will invoke the function immediatley after we require it into this file.
+const routes = require('next-routes')();
+
+// routes.add('...', '...');
+
+module.exports = routes;
