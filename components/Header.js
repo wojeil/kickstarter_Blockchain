452c98840857193015a02c7d86d931ff2924003a
Wadih Ojeil <wadih.ojeil@gmail.com>
2021-11-11 20:43:18 -0500

    added link to header using routesjs Link

--- components/Header.js
+++ components/Header.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import {Menu} from 'semantic-ui-react';
+import {Link} from '../routes';
 
 
 const Header = (props) => {
     return (
         <Menu style={{marginTop: '15px'}}>
-            <Menu.Item> 
-                WadzCoin
-            </Menu.Item>
+            <Link route="/">
+                <a className="item">
+                    WaddzCoin
+                </a>
+            </Link>
             <Menu.Menu position="right">
             <Menu.Item> 
                 Campaigns
