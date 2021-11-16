7d8e5082b7d5a21d9c16e3166061d3d70256a58e
Wadih Ojeil <wadih.ojeil@gmail.com>
2021-11-16 18:56:54 -0500

    attempting to push code

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -4,7 +4,7 @@ import CampingFactory from './build/CampaignFactory.json';
 
 const instance = new web3.eth.Contract(
     JSON.parse(CampingFactory.interface),
-    '0x2400764807f8b62702365ef7Dd54777053D645A9'
+    '0xA404af092BBBA1A6840DcBC2317eb2727298DB86'
 
 );
 
