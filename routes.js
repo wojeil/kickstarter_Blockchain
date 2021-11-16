cb62549db1f6cdeb80b6853bf32ee58a74d70e49
Wadih Ojeil <wadih.ojeil@gmail.com>
2021-11-15 19:23:08 -0500

    added a new method to direct dynamic routing

--- routes.js
+++ routes.js
@@ -2,7 +2,9 @@
 //require returns a function and the set of paranthesis after will invoke the function immediatley after we require it into this file.
 const routes = require('next-routes')();
 
-// routes.add('...', '...');
+
+//method to add dynamic routing first argument describes rout and second will be the component I tap into
+routes.add('/campaigns/:address', '/campaigns/show');
 
 module.exports = routes;
 
