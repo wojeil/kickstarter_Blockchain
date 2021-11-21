8264c30e68cf3baad87e35cefc04d917a6e14dc4
Wadih Ojeil <wadih.ojeil@gmail.com>
2021-11-21 13:47:56 -0500

    form works and contribute function is sending money to exact address

--- components/ContributeForm.js
+++ components/ContributeForm.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Button, Form, Input, Message } from 'semantic-ui-react';
 import Campaign from '../ethereum/campaign';
+import web3 from '../ethereum/web3';
 
 
 
@@ -10,9 +11,19 @@ class ContributeForm extends Component {
         value: ''
     };
 
-    onSubmit=(event) =>{
+    onSubmit= async event =>{
         event.preventDefault();
        const campaign= Campaign(this.props.address);
+
+       try{
+        const accounts = await web3.eth.getAccounts();
+        await campaign.methods.contribute().send({
+            from: accounts[0],
+            value: web3.utils.toWei(this.state.value, 'ether')
+        });
+       }catch(err){
+
+       }
 
     };
 
