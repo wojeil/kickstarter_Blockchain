56f5b88781d196f1a85c36f60c04687f59359b18
Wadih Ojeil <wadih.ojeil@gmail.com>
2021-11-21 14:05:25 -0500

    set up the spinner and err handling message

--- components/ContributeForm.js
+++ components/ContributeForm.js
@@ -9,12 +9,15 @@ import {Router} from '../routes';
 class ContributeForm extends Component {
 
     state ={
-        value: ''
+        value: '',
+        errorMessage: '',
+        loading: false
     };
 
     onSubmit= async event =>{
         event.preventDefault();
        const campaign= Campaign(this.props.address);
+       this.setState({loading:true, errorMessage:''});
 
        try{
         const accounts = await web3.eth.getAccounts();
@@ -24,15 +27,18 @@ class ContributeForm extends Component {
         });
         Router.replaceRoute(`/campaigns/${this.props.address}`)
        }catch(err){
+           this.setState({errorMessage:err.message});
 
        }
+
+       this.setState({loading: false, value:''});
 
     };
 
     render(){
 
         return (
-            <Form onSubmit={this.onSubmit}>
+            <Form onSubmit={this.onSubmit} error={!!this.state.errorMessage}>
                 <Form.Field>
                     <label> Amount to Contribute </label>
                     <Input
@@ -42,7 +48,9 @@ class ContributeForm extends Component {
                         labelPosition="right"
                     />
                 </Form.Field>
-                <Button primary>
+
+                <Message error header="Oh Ohhh" content={this.state.errorMessage} />
+                <Button primary loading={this.state.loading}>
                     Contribute
                 </Button>
             </Form>
