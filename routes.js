7d2abb9e635b53aa9629369f0ddfe50d8f2b17a3
Wadih Ojeil <wadih.ojeil@gmail.com>
2021-11-21 17:22:16 -0500

    fixed and replaced routes file

--- routes.js
+++ routes.js
@@ -1,18 +1,9 @@
+const routes = require("next-routes")();
 
-//require returns a function and the set of paranthesis after will invoke the function immediatley after we require it into this file.
-const routes = require('next-routes')();
-
-routes.add('/campaigns/new', '/campaigns/new')
-//method to add dynamic routing first argument describes rout and second will be the component I tap into
-.add('/campaigns/:address', '/campaigns/show')
-
-//method to add a new route towards the list of requests page 
-.add('/campaigns/:address/requests', '/campaigns/requests/index')
-
-//adding Route for the new js for requests
-.add('campaigns/:address/requests/new', 'campaigns/requests/new');
+routes
+  .add("/campaigns/new", "/campaigns/new")
+  .add("/campaigns/:address", "/campaigns/show")
+  .add("/campaigns/:address/requests", "/campaigns/requests/index")
+  .add("/campaigns/:address/requests/new", "/campaigns/requests/new");
 
 module.exports = routes;
-
-
-
