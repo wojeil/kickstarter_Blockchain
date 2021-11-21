6c2b154a3a16191382d87e567fea0fb799c542ac
Wadih Ojeil <wadih.ojeil@gmail.com>
2021-11-21 16:07:14 -0500

    creating the form to create a request

--- pages/campaigns/requests/new.js
+++ pages/campaigns/requests/new.js
@@ -1,13 +1,53 @@
 import React, {Component} from 'react';
+import { Form, Button, Message, Input } from 'semantic-ui-react';
+import Campaign from '../../../ethereum/campaign';
+import web3 from '../../../ethereum/web3';
+import { Link, Router } from '../../../routes';
+import Layout from '../../../components/Layout';
 
 
 
 class RequestNew extends Component{
 
+    static async getInitialProps(props){
+        const {address}=props.query;
+
+
+        return {address};
+    }
+
     render(){
 
         return(
-            <h3>Create Request</h3>
+            <Layout>
+                <h3>Create Request</h3>
+            <Form>
+                <Form.Field>
+                    <label>
+                        Description
+                    </label>
+                    <Input/>
+                </Form.Field>
+
+                <Form.Field>
+                    <label>
+                        Value in Ether
+                    </label>
+                    <Input/>
+                </Form.Field>
+
+                <Form.Field>
+                    <label>
+                        Recipient
+                    </label>
+                    <Input/>
+                </Form.Field>
+
+            </Form>
+
+            </Layout>
+            
+           
         );
     }
 }
