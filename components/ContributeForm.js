af016feaea040a5ffc903e0083197cc9250a27da
Wadih Ojeil <wadih.ojeil@gmail.com>
2021-11-21 13:01:08 -0500

    setting up the contribute form component

---
+++ components/ContributeForm.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import { Form, Input, Message } from 'semantic-ui-react';
+
+
+class ContributeForm extends Component {
+    render(){
+
+        return (
+            <Form>
+                <Form.Field>
+                    <label> Amount to Contribute </label>
+                    <Input
+                        
+                    />
+                </Form.Field>
+            </Form>
+        );
+
+    }
+
+}
+
+export default ContributeForm;
