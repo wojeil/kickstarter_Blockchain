65639d002b65669e4094c743163feba31040e609
Wadih Ojeil <wadih.ojeil@gmail.com>
2021-11-21 19:02:58 -0500

    imported campaign and request count from solidity and set up a loop to go over each request in the current campaign

--- pages/campaigns/requests/index.js
+++ pages/campaigns/requests/index.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import Layout from '../../../components/Layout';
 import {Button} from 'semantic-ui-react';
 import {Link} from '../../../routes';
+import Campaign from'../../../ethereum/campaign';
 
 
 
@@ -10,6 +11,16 @@ class RequestIndex extends Component {
 
     static async getInitialProps(props){
         const {address} = props.query;
+        const campaign =Campaign(address);
+        const requestCount = await campaign.methods.getRequestsCount().call();
+
+        const request = await Promise.all(
+            Array(requestCount).fill().map((element, index) =>{
+                return campaign.methods.requests(index).call()
+            })
+        );
+
+        console.log(requests);
 
         return {address};
     }
