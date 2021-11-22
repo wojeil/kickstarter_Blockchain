37104c29e2f724caa7e97272c839242ca0702e67
Wadih Ojeil <wadih.ojeil@gmail.com>
2021-11-21 20:23:41 -0500

    created a helper method render row that we can call in the table body

--- pages/campaigns/requests/index.js
+++ pages/campaigns/requests/index.js
@@ -3,6 +3,7 @@ import Layout from '../../../components/Layout';
 import {Button, Table} from 'semantic-ui-react';
 import {Link} from '../../../routes';
 import Campaign from'../../../ethereum/campaign';
+import RequestRow from '../../../components/RequestRow';
 
 
 
@@ -23,6 +24,18 @@ class RequestIndex extends Component {
         console.log(requests);
 
         return {address, requests, requestCount};
+    }
+
+    RenderRow(){
+        return this.props.requests.map((request, index) => {
+
+            return <RequestRow
+                key={index}
+                request={request}
+                address={this.props.address}
+                 />;
+
+        });
     }
 
     render(){
@@ -51,6 +64,9 @@ class RequestIndex extends Component {
                             <HeaderCell>Finalize</HeaderCell>
                         </Row>
                     </Header>
+                    <Body>
+                        {this.RenderRow()}
+                    </Body>
                 </Table>     
             </Layout>
             
