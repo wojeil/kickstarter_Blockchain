628e446daff26646cd962c1fde9bbfa4da5b19be
Wadih Ojeil <wadih.ojeil@gmail.com>
2021-11-21 19:22:58 -0500

    setting up the table

--- pages/campaigns/requests/index.js
+++ pages/campaigns/requests/index.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import Layout from '../../../components/Layout';
-import {Button} from 'semantic-ui-react';
+import {Button, Table} from 'semantic-ui-react';
 import {Link} from '../../../routes';
 import Campaign from'../../../ethereum/campaign';
 
@@ -27,6 +27,8 @@ class RequestIndex extends Component {
 
     render(){
 
+        const { Header, Row, HeaderCell, Body } = Table;
+
         return(
             <Layout>
                 <h3>Requests</h3> 
@@ -36,7 +38,20 @@ class RequestIndex extends Component {
                             Add Requests
                         </Button>
                     </a>
-                </Link>      
+                </Link> 
+                <Table>
+                    <Header>
+                        <Row>
+                            <HeaderCell>ID</HeaderCell>
+                            <HeaderCell>Description</HeaderCell>
+                            <HeaderCell>Amount</HeaderCell>
+                            <HeaderCell>Recipient</HeaderCell>
+                            <HeaderCell>Approval</HeaderCell>
+                            <HeaderCell>Approve</HeaderCell>
+                            <HeaderCell>Finalize</HeaderCell>
+                        </Row>
+                    </Header>
+                </Table>     
             </Layout>
             
         );
