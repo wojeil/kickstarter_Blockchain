fd71f5595bba816b5eea836a58b02dea54c6b602
Wadih Ojeil <wadih.ojeil@gmail.com>
2021-11-21 20:55:36 -0500

    added a button

--- components/RequestRow.js
+++ components/RequestRow.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Table} from 'semantic-ui-react';
+import {Table, Button} from 'semantic-ui-react';
 import web3 from '../ethereum/web3';
 
 
@@ -18,7 +18,9 @@ class RequestRow extends Component{
                 <Cell>{web3.utils.fromWei(request.value, 'ether')}</Cell>
                 <Cell>{request.recipient}</Cell>
                 <Cell>{request.approvalCount}/{approversCount}</Cell>
-                <Cell></Cell>
+                <Cell>
+                    <Button color="green" basic>Approve</Button>
+                </Cell>
                 <Cell></Cell>
             </Row>
         );
