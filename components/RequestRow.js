a0a55eee571407c5e78357b33f999f67037c3ba4
Wadih Ojeil <wadih.ojeil@gmail.com>
2021-11-21 19:29:43 -0500

    setting up a new component for dynamic row rendering

---
+++ components/RequestRow.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+
+
+class RequestRow extends Component{
+
+    render(){
+
+        return(
+            <div>Request!</div>
+        );
+    }
+}
+
+
+export default RequestRow;
