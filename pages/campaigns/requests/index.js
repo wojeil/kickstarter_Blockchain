e5f62991b6fdff57edfb25a18e66bf37a76ff619
Wadih Ojeil <wadih.ojeil@gmail.com>
2021-11-21 19:11:39 -0500

    parse int added because the Array constructor expects to be passed a number and not a string

--- pages/campaigns/requests/index.js
+++ pages/campaigns/requests/index.js
@@ -15,14 +15,14 @@ class RequestIndex extends Component {
         const requestCount = await campaign.methods.getRequestsCount().call();
 
         const requests = await Promise.all(
-            Array(requestCount).fill().map((element, index) =>{
+            Array(parseInt(requestCount)).fill().map((element, index) =>{
                 return campaign.methods.requests(index).call()
             })
         );
 
         console.log(requests);
 
-        return {address, requests};
+        return {address, requests, requestCount};
     }
 
     render(){
